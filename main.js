d1723465f9d365f579da2187d3833159ad03d0d2
marcusespino <marcusespino2001@gmail.com>
2025-11-09 18:01:45 -0300

    atualizando exercicio

--- main.js
+++ main.js
@@ -1,30 +1,57 @@
-function Device(tipo, fabricante){
-    this.tipo = tipo;
-    this.fabricante = fabricante;
+class Device {
+    constructor(tipo, fabricante) {
+        if (this.constructor === Device) {
+            throw new Error("A classe Device é abstrata e não pode ser instanciada diretamente.");
+        }
+        this.tipo = tipo;
+        this.fabricante = fabricante;
+    }
+
+    mostrarInfo() {
+        return `Tipo: ${this.tipo} | Fabricante: ${this.fabricante}`;
+    }
 }
 
-function Celular(tipo, fabricante, modelo, preco){
-    Device.call(this, tipo, fabricante);
-    this.modelo = modelo;
-    this.preco = preco;
+class Celular extends Device {
+    constructor(tipo, fabricante, modelo, preco) {
+        super(tipo, fabricante);
+        this.modelo = modelo;
+        this.preco = preco;
+    }
+
+    mostrarInfo() {
+        return `${super.mostrarInfo()} | Modelo: ${this.modelo} | Preço: R$${this.preco}`;
+    }
 }
 
-function Computador(tipo, fabricante, modelo, preco){
-    Device.call(this, tipo, fabricante);
-    this.modelo = modelo;
-    this.preco = preco;
+class Computador extends Device {
+    constructor(tipo, fabricante, modelo, preco) {
+        super(tipo, fabricante);
+        this.modelo = modelo;
+        this.preco = preco;
+    }
+
+    mostrarInfo() {
+        return `${super.mostrarInfo()} | Modelo: ${this.modelo} | Preço: R$${this.preco}`;
+    }
 }
 
-function Tablet(tipo, fabricante, modelo, preco){
-    Device.call(this, tipo, fabricante);
-    this.modelo = modelo;
-    this.preco = preco;
+class Tablet extends Device {
+    constructor(tipo, fabricante, modelo, preco) {
+        super(tipo, fabricante);
+        this.modelo = modelo;
+        this.preco = preco;
+    }
+
+    mostrarInfo() {
+        return `${super.mostrarInfo()} | Modelo: ${this.modelo} | Preço: R$${this.preco}`;
+    }
 }
 
 const celular1 = new Celular("Smartphone", "Samsung", "A19", 2299.90);
 const computador1 = new Computador("Desktop", "Lenovo", "3i", 8349.90);
-const tablet1 = new Tablet("iPad", "Apple", "10th generation", 8665,90);
+const tablet1 = new Tablet("Tablet", "Apple", "10th generation", 8665.90);
 
-console.log(celular1);
-console.log(computador1);
-console.log(tablet1);
+console.log(celular1.mostrarInfo());
+console.log(computador1.mostrarInfo());
+console.log(tablet1.mostrarInfo());
